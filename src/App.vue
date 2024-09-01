ac4213c168f022f5d3b5ddd8719f56ac7206fed2
Salman-sajib <sfsajib@gmail.com>
2024-09-01 11:30:11 +0600

    mouse track composable

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useMouse } from "./mouse";
+
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <h1 class="text-4xl">Vue3 Composables</h1>
+
+  <div>
+    <h2>Track Mouse Position</h2>
+    <p>Position X: {{ x }}</p>
+    <p>Position Y: {{ y }}</p>
+  </div>
+</template>
